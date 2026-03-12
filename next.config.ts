6cd41cf25e719a0b535805342b9d05a7357849d5
Eduardo Corveloni N. <eddycn3@gmail.com>
2026-03-11 22:15:18 -0300

    content layer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+// next.config.js
+const { withContentlayer } = require('next-contentlayer')
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  turbopack: {}, // <- adiciona isso
+}
 
-export default nextConfig;
+module.exports = withContentlayer(nextConfig)
